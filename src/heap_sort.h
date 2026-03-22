22d7a738bbe9821786ee32e21ab8020623daf892
Juan Gómez <jmgomezr@eafit.edu.co>
2026-03-21 20:26:49 -0500

    Bug fix: Tipo de retorno correcto de estimate_memory_mb de void a double

--- src/heap_sort.h
+++ src/heap_sort.h
@@ -7,5 +7,4 @@ void heapify(std::vector<std::string>& arr, int n, int i);
 void build_max_heap(std::vector<std::string>& arr);
 
 void heap_sort(std::vector<std::string>& arr);
-
-void estimate_memory_mb(const std::vector<std::string>& arr);
+double estimate_memory_mb(const std::vector<std::string>& arr);
